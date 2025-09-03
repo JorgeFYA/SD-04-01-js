28bb201a756e749e617f2771af82981f9a77e7b5
Francisco Yañez <jorgeyanezar@gmail.com>
2025-09-03 16:32:20 -0600

    Taks 1-7 completed

--- src/task6.js
+++ src/task6.js
@@ -3,6 +3,8 @@ const arr2 = ["world"]
 
 // Type your code below this line!
 
+const completeWord = arr1.concat(arr2);
+console.log(completeWord);
 
 
 // Type your code above this line!
--- src/task7.js
+++ src/task7.js
@@ -5,7 +5,22 @@ const arr = [
   ]
   
   // Type your code below this line!
+  console.log("\n¿Pueden agregar un solo número a una fila existente?");
+  arr[2].push(30);
+  console.log(arr);
+
+  console.log("\n¿Pueden agregar una fila completa de números?");
+  arr.push([31,32,33,34,35,36,37,38,39]);
+  console.log(arr);
+
+  //Elimino nuevamente el 30
+  //arr[2].pop(); // muy sencillo porqye está al final
+  console.log("\n¿Pueden eliminar un solo número de una fila específica?");
+  arr[2].splice(10,1);
+  console.log(arr);
   
-  
-  
+
+  console.log("\n¿Pueden invertir una de las filas sin afectar a las demás?");
+  arr[3].reverse();
+  console.log(arr);
   // Type your code above this line!
